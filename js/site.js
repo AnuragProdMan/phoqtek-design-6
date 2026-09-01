b06acfe2304cc7bd0e912b3c67bea8f3e2b82d06
Anurag <anuragsingh805263@gmail.com>
2026-09-02 02:37:53 +0530

    Point the Rafales at Earth.
    The jet sequence is now a steep dive, noses down toward the planet.

--- js/site.js
+++ js/site.js
@@ -100,7 +100,7 @@
     'CONVOY <b>IN MOTION</b>',
     'GNSS <b class="warn">LOCK → DENIED</b>',
     'UAV <b>IN FLIGHT</b>',
-    'RAFALE <b>ON APPROACH</b>',
+    'RAFALE <b>NOSE DOWN</b>',
     'GROUND · VNS <b>TERRAIN LOCK</b>'
   ];
 
